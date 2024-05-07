d19e08f4e0bfecd965a4ebfa3a5b9b9854d9081c
kelvin defrNBAH <kevinocholah@gmail.com>
2024-05-07 05:20:44 +0300

    tarball prep

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module contains the function do_pack that generates a .tgz archive
+  from the contents of the web_static folder (fabric script) """
+
+import os.path
+from fabric.api import *
+from datetime import datetime   
+
+
+@task1(
+    def do_pack():
+    """ Fabric script that generates a .tgz archive from the contents of the...
+    ...web_static folder """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
+)
+
